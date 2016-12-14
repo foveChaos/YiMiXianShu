c78a6f1b15664e4572a41a1f27549d087ed4bf92
Sun Chao <Pactera@SundeMac-mini.local>
2016-12-14 09:28:59 +0800

    no message

--- YiMiXianShu/Library/TabBarControllerProtocol.swift
+++ YiMiXianShu/Library/TabBarControllerProtocol.swift
@@ -11,10 +11,47 @@ import UIKit
 
 protocol TabBarControllerProtocol {
     
+    func makeControllers()
+    
     func modifyTabBarItem(settings: TabBarItemSettings) -> UIViewController
 }
 
 extension TabBarControllerProtocol where Self: UITabBarController {
+    
+    func makeControllers() {
+        
+        var controllers = [UIViewController]()
+        
+        var homeSettings = TabBarItemSettings(storyboardName: "Home", viewControllerName: "HomeViewController")
+        homeSettings.title = "首页"
+        homeSettings.image = UIImage.imageWithOriginalName(named: "ico_viewgallery_normal")
+        homeSettings.selectedImage = UIImage.imageWithOriginalName(named: "ico_viewgallery_selected")
+        let homeViewController = modifyTabBarItem(settings: homeSettings)
+        controllers.append(homeViewController)
+        
+        var listSettings = TabBarItemSettings(storyboardName: "List", viewControllerName: "ListViewController")
+        listSettings.title = "分类"
+        listSettings.image = UIImage.imageWithOriginalName(named: "ico_category_normal")
+        listSettings.selectedImage = UIImage.imageWithOriginalName(named: "ico_category_selected")
+        let listViewController = modifyTabBarItem(settings: listSettings)
+        controllers.append(listViewController)
+        
+        var cartSettings = TabBarItemSettings(storyboardName: "Cart", viewControllerName: "CartViewController")
+        cartSettings.title = "购物车"
+        cartSettings.image = UIImage.imageWithOriginalName(named: "ico_cart_normal")
+        cartSettings.selectedImage = UIImage.imageWithOriginalName(named: "ico_cart_selected")
+        let cartViewController = modifyTabBarItem(settings: cartSettings)
+        controllers.append(cartViewController)
+        
+        var mineSettings = TabBarItemSettings(storyboardName: "Mine", viewControllerName: "MineViewController")
+        mineSettings.title = "我的"
+        mineSettings.image = UIImage.imageWithOriginalName(named: "ico_account_normal")
+        mineSettings.selectedImage = UIImage.imageWithOriginalName(named: "ico_account_selected")
+        let mineViewController = modifyTabBarItem(settings: mineSettings)
+        controllers.append(mineViewController)
+        
+        setViewControllers(controllers, animated: true)
+    }
     
     func modifyTabBarItem(settings: TabBarItemSettings) -> UIViewController {
         
@@ -34,6 +71,11 @@ extension TabBarControllerProtocol where Self: UITabBarController {
         }
         
         return viewController
+    }
+    
+    func setTitleColor(color: UIColor, _ selectedColor: UIColor) {
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: color], for: UIControlState.normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: selectedColor], for: UIControlState.normal)
     }
 }
 
--- YiMiXianShu/View/AnimatedTabBarController.swift
+++ YiMiXianShu/View/AnimatedTabBarController.swift
@@ -9,48 +9,18 @@
 import Foundation
 import UIKit
 
-class AnimatedTabBarController: UITabBarController, ViewControllerProtocol, TabBarControllerProtocol {
+final class AnimatedTabBarController: UITabBarController, ViewControllerProtocol, TabBarControllerProtocol {
     
     override func viewDidLoad() {
-        setViewControllers(makeControllers(), animated: true)
+        
+
     }
 }
 
 extension AnimatedTabBarController {
     
-    func makeControllers() -> [UIViewController] {
-        
-        var controllers = [UIViewController]()
-        
-        var homeSettings = TabBarItemSettings(storyboardName: "Home", viewControllerName: "HomeViewController")
-        homeSettings.title = "首页"
-        homeSettings.image = UIImage.imageWithOriginalName(named: "ico_viewgallery_normal")
-        homeSettings.selectedImage = UIImage.imageWithOriginalName(named: "ico_viewgallery_selected")
-        let homeViewController = modifyTabBarItem(settings: homeSettings)
-        controllers.append(homeViewController)
-        
-        var listSettings = TabBarItemSettings(storyboardName: "List", viewControllerName: "ListViewController")
-        listSettings.title = "分类"
-        listSettings.image = UIImage.imageWithOriginalName(named: "ico_category_normal")
-        listSettings.selectedImage = UIImage.imageWithOriginalName(named: "ico_category_selected")
-        let listViewController = modifyTabBarItem(settings: listSettings)
-        controllers.append(listViewController)
-        
-        var cartSettings = TabBarItemSettings(storyboardName: "Cart", viewControllerName: "CartViewController")
-        cartSettings.title = "购物车"
-        cartSettings.image = UIImage.imageWithOriginalName(named: "ico_cart_normal")
-        cartSettings.selectedImage = UIImage.imageWithOriginalName(named: "ico_cart_selected")
-        let cartViewController = modifyTabBarItem(settings: cartSettings)
-        controllers.append(cartViewController)
-        
-        var mineSettings = TabBarItemSettings(storyboardName: "Mine", viewControllerName: "MineViewController")
-        mineSettings.title = "我的"
-        mineSettings.image = UIImage.imageWithOriginalName(named: "ico_account_normal")
-        mineSettings.selectedImage = UIImage.imageWithOriginalName(named: "ico_account_selected")
-        let mineViewController = modifyTabBarItem(settings: mineSettings)
-        controllers.append(mineViewController)
-        
-        return controllers
+    func initialize() {
+        makeControllers()
     }
 }
 
@@ -58,13 +28,5 @@ extension AnimatedTabBarController: UITabBarControllerDelegate {
     
     override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
         print("didSelect")
-    }
-    
-    override func tabBar(_ tabBar: UITabBar, willBeginCustomizing items: [UITabBarItem]) {
-        print("willBeginCustomizing")
-    }
-    
-    override func tabBar(_ tabBar: UITabBar, didBeginCustomizing items: [UITabBarItem]) {
-        print("didBeginCustomizing")
     }
 }
