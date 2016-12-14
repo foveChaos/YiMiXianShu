26ddf88a9d6ab1447f2f0e48bd29962bce24e954
Sun Chao <Pactera@SundeMac-mini.local>
2016-12-14 16:21:44 +0800

    update

--- YiMiXianShu/Library/TabBarControllerProtocol.swift
+++ YiMiXianShu/Library/TabBarControllerProtocol.swift
@@ -19,50 +19,17 @@ extension TabBarControllerProtocol where Self: UITabBarController {
     func makeControllers() {
         
         var controllers = [UIViewController]()
-        
-        var homeSettings = TabBarItemSettings(storyboardName: "Home", viewControllerName: "HomeViewController")
-        homeSettings.title = "首页"
-        homeSettings.image = UIImage.imageWithOriginalName(named: "ico_viewgallery_normal")
-        homeSettings.selectedImage = UIImage.imageWithOriginalName(named: "ico_viewgallery_selected")
-        let homeViewController = modifyTabBarItem(settings: homeSettings)
-        let homeNavigation = UINavigationController(rootViewController: homeViewController)
-        homeNavigation.hidesBottomBarWhenPushed = true
-        controllers.append(homeNavigation)
-        
-        var listSettings = TabBarItemSettings(storyboardName: "List", viewControllerName: "ListViewController")
-        listSettings.title = "分类"
-        listSettings.image = UIImage.imageWithOriginalName(named: "ico_category_normal")
-        listSettings.selectedImage = UIImage.imageWithOriginalName(named: "ico_category_selected")
-        let listViewController = modifyTabBarItem(settings: listSettings)
-        let listNavigation = UINavigationController(rootViewController: listViewController)
-        listNavigation.hidesBottomBarWhenPushed = true
-        controllers.append(listNavigation)
-        
-        var cartSettings = TabBarItemSettings(storyboardName: "Cart", viewControllerName: "CartViewController")
-        cartSettings.title = "购物车"
-        cartSettings.image = UIImage.imageWithOriginalName(named: "ico_cart_normal")
-        cartSettings.selectedImage = UIImage.imageWithOriginalName(named: "ico_cart_selected")
-        let cartViewController = modifyTabBarItem(settings: cartSettings)
-        let cartNavigation = UINavigationController(rootViewController: cartViewController)
-        cartNavigation.hidesBottomBarWhenPushed = true
-        controllers.append(cartNavigation)
-        
-        var mineSettings = TabBarItemSettings(storyboardName: "Mine", viewControllerName: "MineViewController")
-        mineSettings.title = "我的"
-        mineSettings.image = UIImage.imageWithOriginalName(named: "ico_account_normal")
-        mineSettings.selectedImage = UIImage.imageWithOriginalName(named: "ico_account_selected")
-        let mineViewController = modifyTabBarItem(settings: mineSettings)
-        let mineNavigation = UINavigationController(rootViewController: mineViewController)
-        mineNavigation.hidesBottomBarWhenPushed = true
-        controllers.append(mineNavigation)
-        
+        controllers.append(createMainScene(name: .home))
+        controllers.append(createMainScene(name: .list))
+        controllers.append(createMainScene(name: .cart))
+        controllers.append(createMainScene(name: .mine))
         setViewControllers(controllers, animated: true)
     }
 }
 
 fileprivate extension TabBarControllerProtocol {
     
-    func modifyTabBarItem(settings: TabBarItemSettings) -> UIViewController {
+    func createRootViewController(settings: TabBarItemSettings) -> UIViewController {
         
         let storyboard = UIStoryboard(name: settings.storyboardName, bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: settings.viewControllerName)
@@ -82,6 +49,19 @@ fileprivate extension TabBarControllerProtocol {
         return viewController
     }
     
+    func createNavigationController(rootViewController: UIViewController) -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        navigationController.hidesBottomBarWhenPushed = true
+        return navigationController
+    }
+    
+    func createMainScene(name: StoryboardName) -> UINavigationController {
+        let settings = TabBarItemSettings(name: name)
+        let viewController = createRootViewController(settings: settings)
+        let navigation = createNavigationController(rootViewController: viewController)
+        return navigation
+    }
+    
     func setTitleColor(color: UIColor, selectedColor: UIColor) {
         
         UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: color], for: UIControlState.normal)
@@ -90,17 +70,53 @@ fileprivate extension TabBarControllerProtocol {
     
 }
 
-struct TabBarItemSettings {
+fileprivate enum StoryboardName: String {
+    case home
+    case list
+    case cart
+    case mine
+}
+
+fileprivate struct TabBarItemSettings {
+    
     var storyboardName: String
     var viewControllerName: String
-    var title: String?
+    var title: String
     var titleColor: UIColor?
     var selectedTitleColor: UIColor?
     var image: UIImage?
     var selectedImage: UIImage?
     
-    init(storyboardName: String, viewControllerName: String) {
-        self.storyboardName = storyboardName
-        self.viewControllerName = viewControllerName
+    init(name: StoryboardName) {
+        
+        if name == .home {
+            storyboardName = "Home"
+            viewControllerName = "HomeViewController"
+            title = "首页"
+            image = UIImage.imageWithOriginalName(named: "ico_viewgallery_normal")
+            selectedImage = UIImage.imageWithOriginalName(named: "ico_viewgallery_selected")
+        } else if name == .list {
+            storyboardName = "List"
+            viewControllerName = "ListViewController"
+            title = "分类"
+            image = UIImage.imageWithOriginalName(named: "ico_category_normal")
+            selectedImage = UIImage.imageWithOriginalName(named: "ico_category_selected")
+        } else if name == .cart {
+            storyboardName = "Cart"
+            viewControllerName = "CartViewController"
+            title = "购物车"
+            image = UIImage.imageWithOriginalName(named: "ico_cart_normal")
+            selectedImage = UIImage.imageWithOriginalName(named: "ico_cart_selected")
+        } else if name == .mine {
+            storyboardName = "Mine"
+            viewControllerName = "MineViewController"
+            title = "我的"
+            image = UIImage.imageWithOriginalName(named: "ico_account_normal")
+            selectedImage = UIImage.imageWithOriginalName(named: "ico_account_selected")
+        } else {
+            storyboardName = ""
+            viewControllerName = ""
+            title = ""
+        }
     }
 }
